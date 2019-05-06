adc5d8f4bd4635d1c33343dd3505ae5dd9075c2f
AhmedAliAhmedMostafa <ahmed_elady42@yahoo.com>
2019-05-07 00:19:06 +0200

    Add initial Drawing Version .

---
+++ assign/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace Assign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.AddProcessPanel = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.StartPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AllocationMethod = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.add_holes = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.panel1.SuspendLayout();
+            this.AddProcessPanel.SuspendLayout();
+            this.StartPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.AddProcessPanel);
+            this.panel1.Controls.Add(this.StartPanel);
+            this.panel1.Controls.Add(this.AllocationMethod);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.add_holes);
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1116, 524);
+            this.panel1.TabIndex = 2;
+            // 
+            // AddProcessPanel
+            // 
+            this.AddProcessPanel.Controls.Add(this.label2);
+            this.AddProcessPanel.Controls.Add(this.button2);
+            this.AddProcessPanel.Controls.Add(this.textBox2);
+            this.AddProcessPanel.Location = new System.Drawing.Point(30, 59);
+            this.AddProcessPanel.Name = "AddProcessPanel";
+            this.AddProcessPanel.Size = new System.Drawing.Size(342, 81);
+            this.AddProcessPanel.TabIndex = 25;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Segment Number";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(22, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 23);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Add Process";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(161, 23);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 20;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // StartPanel
+            // 
+            this.StartPanel.Controls.Add(this.button1);
+            this.StartPanel.Controls.Add(this.textBox3);
+            this.StartPanel.Controls.Add(this.label3);
+            this.StartPanel.Controls.Add(this.textBox1);
+            this.StartPanel.Controls.Add(this.label1);
+            this.StartPanel.Location = new System.Drawing.Point(30, 8);
+            this.StartPanel.Name = "StartPanel";
+            this.StartPanel.Size = new System.Drawing.Size(663, 45);
+            this.StartPanel.TabIndex = 24;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(547, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Confirm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(284, 16);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(224, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "MaxSize";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "#Holes";
+            // 
+            // AllocationMethod
+            // 
+            this.AllocationMethod.AutoSize = true;
+            this.AllocationMethod.Location = new System.Drawing.Point(759, 453);
+            this.AllocationMethod.Name = "AllocationMethod";
+            this.AllocationMethod.Size = new System.Drawing.Size(109, 13);
+            this.AllocationMethod.TabIndex = 23;
+            this.AllocationMethod.Text = "Allocation Method";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "First Fit",
+            "Best Fit"});
+            this.comboBox1.Location = new System.Drawing.Point(881, 450);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 22;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1027, 448);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Allocate";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // add_holes
+            // 
+            this.add_holes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.add_holes.AutoSize = true;
+            this.add_holes.Location = new System.Drawing.Point(1027, 488);
+            this.add_holes.Name = "add_holes";
+            this.add_holes.Size = new System.Drawing.Size(86, 23);
+            this.add_holes.TabIndex = 15;
+            this.add_holes.Text = "Add Holes";
+            this.add_holes.UseVisualStyleBackColor = true;
+            this.add_holes.Click += new System.EventHandler(this.add_process_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(72, 146);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(930, 298);
+            this.tableLayoutPanel1.TabIndex = 14;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button5.Location = new System.Drawing.Point(830, 42);
+            this.button5.Name = "button5";
+            this.button5.Padding = new System.Windows.Forms.Padding(5);
+            this.button5.Size = new System.Drawing.Size(172, 45);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Next";
+            this.button5.UseMnemonic = false;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1169, 558);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.AddProcessPanel.ResumeLayout(false);
+            this.AddProcessPanel.PerformLayout();
+            this.StartPanel.ResumeLayout(false);
+            this.StartPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button add_holes;
+        public System.Windows.Forms.ErrorProvider errorProvider1;
+        public System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label AllocationMethod;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel StartPanel;
+        private System.Windows.Forms.Panel AddProcessPanel;
+    }
+}
+
---
+++ assign/Gant_chart.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assign
+{
+    public partial class Gant_chart : Form
+    {
+      
+        
+        //Pen outer = new Pen(Color.Black,1);
+        Pen Pen_hole = new Pen(Color.Green);
+        Pen pen_old = new Pen(Color.Gray);
+        Pen pen_new = new Pen(Color.Red);
+        Font font_data = new Font(FontFamily.GenericMonospace, 10);
+        Font font_adress = new Font(FontFamily.GenericSerif, 10);
+        Brush brush_adress=new SolidBrush(Color.Red);
+        Brush brush_data = new SolidBrush(Color.Blue);
+        //float outer_width = 2;
+        int memory_width =40;
+        float memory_height=220;
+        float step = 220/ (float)Form1.memory_max_size;
+        int num_adress_digits=(Form1.memory_max_size.ToString()).Length;
+        int digit_width=2;
+        int digit_height = 4;
+        float starting_x;
+        float starting_y;
+        float w, h;
+        Point genesis_point;
+        List<int> old_start=new List<int>();
+        List<int> old_size=new List<int>();
+        List<int> new_start=new List<int>();
+        List<int> new_size=new List<int>();
+        SortedDictionary<int, KeyValuePair<char, int>> all = new SortedDictionary<int, KeyValuePair<char, int>>();
+        public Gant_chart()
+        {
+            InitializeComponent();
+
+
+        }
+        public string format(string d)
+        {
+            int l = num_adress_digits-d.Length;
+            string n="";
+            for (int i = 0; i < l; i++)
+            { n += "0"; }
+            return n + d;
+               
+
+        }
+        public Point Draw_Area(Graphics g, char type, Point genesis_point,int start,int size,string data)
+        {
+            g.PageUnit = GraphicsUnit.Millimeter;
+            float width=memory_width;
+            float height=size*step;
+            float y = genesis_point.Y;
+            float x = genesis_point.X;
+            genesis_point.Y+=height;
+            float adress_x = starting_x - num_adress_digits * digit_width;
+            float adress_y ;
+            int len=data.Length;
+            float data_x=starting_x+memory_width/2-(len/2*digit_width);
+            float data_y=starting_y+y+height/2;
+            start = int.Parse( format(start.ToString()));
+
+            
+            switch (type)
+            {
+                case'h':
+                    g.DrawRectangle(Pen_hole, x, y, width,height);
+                    g.DrawString(start.ToString(),font_adress,brush_adress,adress_x,adress_y);
+                    g.DrawString(data,font_data,brush_data,data_x,data_y);
+                    break;
+                case'o':
+                    g.DrawRectangle(pen_old, x, y, width,height);
+                    g.DrawString(start.ToString(),font_adress,brush_adress,adress_x,adress_y);
+                    g.DrawString(data,font_data,brush_data,data_x,data_y);
+                    break;
+                case'n':
+                    g.DrawRectangle(pen_new, x, y, width,height);
+                    g.DrawString(start.ToString(),font_adress,brush_adress,adress_x,adress_y);
+                    g.DrawString(data,font_data,brush_data,data_x,data_y);
+                    break;
+                    
+            }
+        }
+        public void sort()
+        { 
+            int total=Form1.holes_num+old_start.Count+new_start.Count;
+
+            for(int i=0;i<Form1.holes_num;i++)
+            {
+                all.Add(Form1.holes_start[i],new KeyValuePair<char,int>('h',i));
+            }
+             for(int i=0;i<old_start.Count;i++)
+            {
+                all.Add(old_start[i], new KeyValuePair<char, int>('o', i));
+            }
+            for(int i=0;i<new_start.Count;i++)
+            {
+                all.Add(new_start[i],new KeyValuePair<char,int>('n',i));
+            }
+
+        }
+        public void Redraw(Graphics g)
+        {
+            g.Clear(Color.White);
+            g.PageUnit = GraphicsUnit.Millimeter;
+            g.DrawRectangle(outer, starting_x, starting_y, memory_width, memory_height);
+            ////////////////Holes/////////////////////////////////
+          
+       
+                for (int i = 0; i < Form1.holes_num; i++)
+                {
+                    Draw_Area(g, 'h', Form1.holes_start[i], Form1.holes_size[i], "Hole" + i.ToString());
+                }
+            ////////////////Old///////////////////////////////////
+                for (int j = 0; j < old_start.Count; j++)
+                {
+                    Draw_Area(g, 'o', old_start[j],old_size[j],"Old"+j.ToString());
+                }
+                ////////////////new///////////////////////////////////
+                for (int k = 0; k< new_start.Count; k++)
+                {
+                    Draw_Area(g, 'n',new_start[k], new_size[k], "x" + k.ToString());
+                }
+            
+        }
+        public void get_old()
+        {
+           
+            int itr=0;
+            foreach(KeyValuePair<int,int>p in Form1.sortedHoles)
+            {
+                if (itr < p.Key)
+                {
+                    old_start.Add(itr);
+                    old_size.Add(p.Key - itr);
+                    itr = p.Key + p.Value;
+                }
+                else
+                {
+                    itr = p.Key + p.Value;
+                }
+            }
+            if (itr != Form1.memory_max_size - 1)
+            {
+                old_start.Add(itr);
+                old_size.Add((Form1.memory_max_size) - itr);
+            }
+        }
+
+
+        private void Gant_chart_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.PageUnit = GraphicsUnit.Millimeter;
+            starting_x =130;
+            starting_y = 0;
+            get_old();
+            Redraw(g);
+        }
+
+
+
+
+
+
+    }
+}
